66a00f5ae4a6d612c1790befc1d219e7c577e444
Lizbeth06 <lizbeth.infante.27@unsch.edu.pe>
2025-11-07 19:17:56 -0500

    cambio 5

---
+++ src/main/java/academiaapi/ipd/gob/pe/academiaapi/repository/ITipoinvolucradoRepository.java
@@ -0,0 +1,7 @@
+package academiaapi.ipd.gob.pe.academiaapi.repository;
+import academiaapi.ipd.gob.pe.academiaapi.model.Tipoinvolucrado;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ITipoinvolucradoRepository extends IGenericRepo<Tipoinvolucrado,Integer>{
+}
