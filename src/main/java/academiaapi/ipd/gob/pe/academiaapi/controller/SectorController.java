84cc12d09ad0da80919aea8b94d673e21e729be8
Jourdy Huamán <jourdyhuamanchuchon@gmail.com>
2025-12-11 12:04:01 -0500

    Agregar anotaciones swagger a los controladores de tipoConvocatoria y Sector

--- src/main/java/academiaapi/ipd/gob/pe/academiaapi/controller/SectorController.java
+++ src/main/java/academiaapi/ipd/gob/pe/academiaapi/controller/SectorController.java
@@ -4,6 +4,8 @@ import academiaapi.ipd.gob.pe.academiaapi.dto.SectorDTO;
 import academiaapi.ipd.gob.pe.academiaapi.model.Sector;
 import academiaapi.ipd.gob.pe.academiaapi.service.ISectorService;
 import academiaapi.ipd.gob.pe.academiaapi.util.MapperUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,22 +19,26 @@ import java.util.List;
 @Controller
 @RequestMapping("/api/sector")
 @RequiredArgsConstructor
+@Tag(name = "tbl_sector")
 public class SectorController {
     private final ISectorService sectorService;
     private final MapperUtil mapperUtil;
 
+    @Operation(summary = "Lista todos los sectores")
     @GetMapping
     public ResponseEntity<List<SectorDTO>> findAll() {
         List<SectorDTO> list = mapperUtil.mapList(sectorService.findAll(), SectorDTO.class);
         return ResponseEntity.ok(list);
     }
 
+    @Operation(summary = "List un rol")
     @GetMapping("/{id}")
     public ResponseEntity<SectorDTO> findById(@PathVariable("id") Integer id) {
         Sector obj = sectorService.findById(id);
         return ResponseEntity.ok(mapperUtil.map(obj, SectorDTO.class));
     }
 
+    @Operation(summary = "Crea un sector")
     @PostMapping
     public ResponseEntity<Void> save(@Valid @RequestBody SectorDTO dto) {
         Sector obj = sectorService.save(mapperUtil.map(dto, Sector.class));
@@ -40,6 +46,7 @@ public class SectorController {
         return ResponseEntity.created(location).build();
     }
 
+    @Operation(summary = "Actualiza un sector")
     @PutMapping("/{id}")
     public ResponseEntity<SectorDTO> update(@Valid @PathVariable("id") Integer id, @RequestBody SectorDTO dto) {
         dto.setIdSector(id);
@@ -47,6 +54,7 @@ public class SectorController {
         return ResponseEntity.ok(mapperUtil.map(obj, SectorDTO.class));
     }
 
+    @Operation(summary = "Elimina un sector")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") Integer id) {
         sectorService.delete(id);
--- src/main/java/academiaapi/ipd/gob/pe/academiaapi/controller/TipoconvocatoriaController.java
+++ src/main/java/academiaapi/ipd/gob/pe/academiaapi/controller/TipoconvocatoriaController.java
@@ -4,6 +4,8 @@ import academiaapi.ipd.gob.pe.academiaapi.dto.TipoconvocatoriaDTO;
 import academiaapi.ipd.gob.pe.academiaapi.model.Tipoconvocatoria;
 import academiaapi.ipd.gob.pe.academiaapi.service.ITipoconvocatoriaService;
 import academiaapi.ipd.gob.pe.academiaapi.util.MapperUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,22 +19,26 @@ import java.util.List;
 @Controller
 @RequestMapping("/api/tipoconvocatoria")
 @RequiredArgsConstructor
+@Tag(name = "tbl_tipoconvocatoria")
 public class TipoconvocatoriaController {
     private final ITipoconvocatoriaService tipoconvocatoriaService;
     private final MapperUtil mapperUtil;
 
+    @Operation(summary = "Lista todos los tipos de convocatoria")
     @GetMapping
     public ResponseEntity<List<TipoconvocatoriaDTO>> findAll() {
         List<TipoconvocatoriaDTO> list = mapperUtil.mapList(tipoconvocatoriaService.findAll(), TipoconvocatoriaDTO.class);
         return ResponseEntity.ok(list);
     }
 
+    @Operation(summary = "Lista un tipo de convocatoria")
     @GetMapping("/{id}")
     public ResponseEntity<TipoconvocatoriaDTO> findById(@PathVariable("id") Integer id) {
         Tipoconvocatoria obj = tipoconvocatoriaService.findById(id);
         return ResponseEntity.ok(mapperUtil.map(obj, TipoconvocatoriaDTO.class));
     }
 
+    @Operation(summary = "Crea un tipo de convocatoria")
     @PostMapping
     public ResponseEntity<Void> save(@Valid @RequestBody TipoconvocatoriaDTO dto) {
         Tipoconvocatoria obj = tipoconvocatoriaService.save(mapperUtil.map(dto, Tipoconvocatoria.class));
@@ -40,6 +46,7 @@ public class TipoconvocatoriaController {
         return ResponseEntity.created(location).build();
     }
 
+    @Operation(summary = "Actualiza un tipo de convocatoria")
     @PutMapping("/{id}")
     public ResponseEntity<TipoconvocatoriaDTO> update(@Valid @PathVariable("id") Integer id, @RequestBody TipoconvocatoriaDTO dto) {
         dto.setIdTipoconvocatoria(id);
@@ -47,6 +54,7 @@ public class TipoconvocatoriaController {
         return ResponseEntity.ok(mapperUtil.map(obj, TipoconvocatoriaDTO.class));
     }
 
+    @Operation(summary = "Elimina una convocatoria")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") Integer id) {
         tipoconvocatoriaService.delete(id);
