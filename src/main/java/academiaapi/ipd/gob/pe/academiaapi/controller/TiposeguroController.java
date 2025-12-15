ec3d5c88f2795b625cf083124abca1a7e411a4c9
Jourdy Huamán <jourdyhuamanchuchon@gmail.com>
2025-12-15 16:30:23 -0500

    Añadir etiquetas swagger dentro de los endpoints de tiposeguro

--- src/main/java/academiaapi/ipd/gob/pe/academiaapi/controller/TiposeguroController.java
+++ src/main/java/academiaapi/ipd/gob/pe/academiaapi/controller/TiposeguroController.java
@@ -4,6 +4,8 @@ import academiaapi.ipd.gob.pe.academiaapi.dto.TiposeguroDTO;
 import academiaapi.ipd.gob.pe.academiaapi.model.Tiposeguro;
 import academiaapi.ipd.gob.pe.academiaapi.service.ITiposeguroService;
 import academiaapi.ipd.gob.pe.academiaapi.util.MapperUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,22 +19,26 @@ import java.util.List;
 @Controller
 @RequestMapping("/api/tiposeguro")
 @RequiredArgsConstructor
+@Tag(name = "tbl_tiposeguro")
 public class TiposeguroController {
     private final ITiposeguroService tiposeguroService;
     private final MapperUtil mapperUtil;
 
+    @Operation(summary = "Lista todos los tipos de seguro")
     @GetMapping
     public ResponseEntity<List<TiposeguroDTO>> findAll() {
         List<TiposeguroDTO> list = mapperUtil.mapList(tiposeguroService.findAll(), TiposeguroDTO.class);
         return ResponseEntity.ok(list);
     }
 
+    @Operation(summary = "Lista un seguro")
     @GetMapping("/{id}")
     public ResponseEntity<TiposeguroDTO> findById(@PathVariable("id") Integer id) {
         Tiposeguro obj = tiposeguroService.findById(id);
         return ResponseEntity.ok(mapperUtil.map(obj, TiposeguroDTO.class));
     }
 
+    @Operation(summary = "Crea un registro de seguro")
     @PostMapping
     public ResponseEntity<Void> save(@Valid @RequestBody TiposeguroDTO dto) {
         Tiposeguro obj = tiposeguroService.save(mapperUtil.map(dto, Tiposeguro.class));
@@ -40,6 +46,7 @@ public class TiposeguroController {
         return ResponseEntity.created(location).build();
     }
 
+    @Operation(summary = "Actualiza un registro de un seguro")
     @PutMapping("/{id}")
     public ResponseEntity<TiposeguroDTO> update(@Valid @PathVariable("id") Integer id, @RequestBody TiposeguroDTO dto) {
         dto.setIdTiposeguro(id);
@@ -47,6 +54,7 @@ public class TiposeguroController {
         return ResponseEntity.ok(mapperUtil.map(obj, TiposeguroDTO.class));
     }
 
+    @Operation(summary = "Elimina un seguro")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") Integer id) {
         tiposeguroService.delete(id);
