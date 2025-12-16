5aefc5a2ee03c1ea8f6c8a9d952565777db9716c
Jourdy Huamán <jourdyhuamanchuchon@gmail.com>
2025-12-16 14:22:42 -0500

    Quitar Involucrado y tipoinvolucrado

--- src/main/java/academiaapi/ipd/gob/pe/academiaapi/dto/HorarioDTO.java
+++ src/main/java/academiaapi/ipd/gob/pe/academiaapi/dto/HorarioDTO.java
@@ -1,22 +1,11 @@
 package academiaapi.ipd.gob.pe.academiaapi.dto;
 
-import academiaapi.ipd.gob.pe.academiaapi.model.Listadisciplina;
-import academiaapi.ipd.gob.pe.academiaapi.model.Temporada;
-import academiaapi.ipd.gob.pe.academiaapi.model.Tipoinvolucrado;
-import academiaapi.ipd.gob.pe.academiaapi.model.Turno;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.Column;
-import jakarta.persistence.ForeignKey;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
--- src/main/java/academiaapi/ipd/gob/pe/academiaapi/repository/ITipoinvolucradoRepository.java
+++
@@ -1,7 +0,0 @@
-package academiaapi.ipd.gob.pe.academiaapi.repository;
-import academiaapi.ipd.gob.pe.academiaapi.model.Tipoinvolucrado;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ITipoinvolucradoRepository extends IGenericRepo<Tipoinvolucrado,Integer>{
-}
